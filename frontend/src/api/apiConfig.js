423197bc9130cf2aac4b30a6e0f8130ca60c0d6b
ZayK1 <zayankasim1@gmail.com>
2025-06-02 01:39:38 +0400

    chore: update API_BASE_URL comments in apiConfig.js for clarity
    - Added commented-out alternative API base URLs for local development and deployment scenarios, improving documentation for future reference.

--- frontend/src/api/apiConfig.js
+++ frontend/src/api/apiConfig.js
@@ -1,1 +1,4 @@
 export const API_BASE_URL = "https://orbital-teamyiz-backend.onrender.com"; 
+//"http://10.0.2.2:5000/auth"; // Android Emulator
+//"http://192.168.0.116:5000/auth"; // For iPhone on local Wi-Fi
+// "https://orbital-teamyiz-backend.onrender.com";  // for deployment
