28ac81183a753a005ca9a9570daf0101f3fd898d
ZayK1 <zayankasim1@gmail.com>
2025-06-13 01:26:10 +0400

    fix: expose app instance for gunicorn

--- backend/app.py
+++ backend/app.py
@@ -66,7 +66,10 @@ def create_app():
 
     return app
 
+
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
+
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
